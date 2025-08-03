55e14740ce5df71579129643449307d659611e6f
jmacwill <jonathan.macwilliams@gmail.com>
2025-08-02 18:32:04 -0600

    oops
    Signed-off-by: jmacwill <jonathan.macwilliams@gmail.com>

---
+++ modules/core/default.nix
@@ -0,0 +1,27 @@
+{inputs, ...}: {
+  imports = [
+    ./boot.nix
+    ./flatpak.nix
+    ./fonts.nix
+    ./greetd.nix
+    ./hardware.nix
+    ./nasModule.nix
+    ./network.nix
+    ./nfs.nix
+    ./nh.nix
+    ./packages.nix
+    ./printing.nix
+    ./security.nix
+    ./services.nix
+    ./starfish.nix
+    ./steam.nix
+    ./stylix.nix
+    ./syncthing.nix
+    ./system.nix
+    ./thunar.nix
+    ./user.nix
+    ./virtualisation.nix
+    ./xserver.nix
+    inputs.stylix.nixosModules.stylix
+  ];
+}
